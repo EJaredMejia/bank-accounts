acba5fa0071c097faf4aed3fb6074a479f10b360
EJaredMejia <jaredmejia4440@gmail.com>
2025-03-16 03:54:15 -0600

    fix: test in ci

--- src/api/bank-accounts/back-accounts.test.ts
+++ src/api/bank-accounts/back-accounts.test.ts
@@ -4,11 +4,8 @@ import { describe, it } from "jsr:@std/testing/bdd";
 import { banksRouter } from "./bank-accounts.routes.ts";
 
 describe("bank accounts", () => {
-  Deno.env.set("DB_FILE_NAME", "file::memory:");
-
-  const app = banksRouter;
-
   it("Should return the correct json", async () => {
+    const app = banksRouter;
     const res = await testClient(app)["bank-accounts"].$get();
 
     expect(await res.json()).toEqual({
