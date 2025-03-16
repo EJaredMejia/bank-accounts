865a0344a993ecfea521447e3ae751a259d49cdd
EJaredMejia <jaredmejia4440@gmail.com>
2025-03-16 16:05:31 -0600

    fix: import

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,7 +1,7 @@
 import { Kysely } from "kysely";
 import { Database } from "./models/database.models.ts";
 
-import { Database as SqliteDatabase } from "jsr:@db/sqlite@0.11";
+import { Database as SqliteDatabase } from "jsr:@db/sqlite";
 import { DenoSqlite3Dialect } from "jsr:@soapbox/kysely-deno-sqlite";
 
 export { Database as Sqlite } from "jsr:@db/sqlite";
