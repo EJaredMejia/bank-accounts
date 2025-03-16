5cd34cf70073d6176c2132f6146349ef7d835e00
EJaredMejia <jaredmejia4440@gmail.com>
2025-03-16 16:07:59 -0600

    fix: import sqlite

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,10 +1,10 @@
 import { Kysely } from "kysely";
 import { Database } from "./models/database.models.ts";
 
-import { Database as SqliteDatabase } from "jsr:@db/sqlite";
-import { DenoSqlite3Dialect } from "jsr:@soapbox/kysely-deno-sqlite";
+import { Database as SqliteDatabase } from "@db/sqlite";
+import { DenoSqlite3Dialect } from "@soapbox/kysely-deno-sqlite";
 
-export { Database as Sqlite } from "jsr:@db/sqlite";
+export { Database as Sqlite } from "@db/sqlite";
 
 const dialect = new DenoSqlite3Dialect({
   database: new SqliteDatabase(Deno.env.get("DB_FILE_NAME") || ":memory:"),
