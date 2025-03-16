e8bc1df0b222c499d35fd7983424212421421081
EJaredMejia <jaredmejia4440@gmail.com>
2025-03-16 16:33:32 -0600

    fix: unit tests

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,16 +1,17 @@
 import { Kysely } from "kysely";
 import { Database } from "./models/database.models.ts";
 
-const dbName = Deno.env.get("DB_FILE_NAME");
+const dbName = Deno.env.get("DB_FILE_NAME") || ":memory:";
 
-const { createClient } = dbName?.includes("file")
-  ? await import("@libsql/client/node")
-  : await import("@libsql/client/web");
+const { createClient } =
+  dbName?.includes("file") || dbName?.includes("memory")
+    ? await import("@libsql/client/node")
+    : await import("@libsql/client/web");
 
 import { LibsqlDialect } from "@libsql/kysely-libsql";
 
 const client = createClient({
-  url: Deno.env.get("DB_FILE_NAME") || ":memory:",
+  url: dbName,
   authToken: Deno.env.get("DB_AUTH_TOKEN"),
 });
 
